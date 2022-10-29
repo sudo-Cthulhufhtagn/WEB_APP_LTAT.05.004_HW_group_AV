d0f0a90d26a7528993ab9ae70b0be94ab0752289
danlizard <dan.kuklyanov@gmail.com>
2022-10-29 21:01:52 +0300

    JSON import start
     - added a function to get JSON; use my key for that website, you might as well

---
+++ js/postfunc.js
@@ -0,0 +1,24 @@
+function getPosts(address){
+	fetch("https://api.jsonstores.com/AA903698701950783488Vror/car/1")
+	.then(response => response.json())
+	.then(json => {
+		for (let i in json.data) {
+			addPostToFeed(json.data[i]);
+		}
+	})
+}
+
+function addPostToFeed(jsonPostData){
+	let postCap = document.getElementById("postCap");
+	let newPost = document.createElement("div");
+
+	// let newPostImg = document.createElement("img")
+	// jsonPostData.hasImg ? newPost.appendChild("img") : //pass; NB! figure out later
+
+	let newPostText = document.createElement("p")
+	let newPostTextData = document.createTextNode(jsonPostData.text);
+	newPostText.append(newPostTextData)
+
+	newPost.appendChild(newPostText);
+	postCap.before(newPost);
+}
